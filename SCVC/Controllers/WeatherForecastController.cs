8709e015c30894aa1b2f8d329282e3332e852e84
Bryan Emanuel Paz Ramirez <mrkobp@localhost.localdomain>
2022-04-14 20:12:29 -0600

    Creando Validacion De Token

--- SCVC/Controllers/WeatherForecastController.cs
+++ SCVC/Controllers/WeatherForecastController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using SCVC.Models;
 
 namespace SCVC.Controllers
 {
@@ -13,29 +14,18 @@ namespace SCVC.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IActionResult Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            Reply dato = new Reply()
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+                result = 1,
+                data = true,
+                message = "Token Valido"
+            };
+
+            return Ok(dato);
+            
         }
     }
 }
